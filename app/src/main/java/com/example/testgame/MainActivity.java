dd9e54c07d98d6057adec6a2a41d597b85c13377
Karthikeyan Muruganandam <karthistudy007@gmail.com>
2020-05-06 14:15:48 +0530

    changes

--- app/src/main/java/com/example/testgame/MainActivity.java
+++ app/src/main/java/com/example/testgame/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.testgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button play = findViewById(R.id.play_button);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callPlay(v);
+            }
+        });
+
+        Button scores = findViewById(R.id.scores_button);
+        scores.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callScores(v);
+            }
+        });
+    }
+
+    public void callPlay(View view){
+        Intent intent = new Intent(this,PlayArea.class);
+        startActivity(intent);
+    }
+
+    public void callScores(View view){
+        Intent intent = new Intent(this,ScoresArea.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/testgame/ScoresArea.java
@@ -0,0 +1,14 @@
+package com.example.testgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScoresArea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scores_area);
+    }
+}
